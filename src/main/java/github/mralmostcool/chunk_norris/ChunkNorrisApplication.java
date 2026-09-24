f47b5eab263faad76a5bb1d80d943777d455935d
Neeraj Gupta <theneerajgupta@gmail.com>
2026-09-24 11:55:14 +0530

    setup: initialized springboot project

---
+++ src/main/java/github/mralmostcool/chunk_norris/ChunkNorrisApplication.java
@@ -0,0 +1,13 @@
+package github.mralmostcool.chunk_norris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChunkNorrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChunkNorrisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/mralmostcool/chunk_norris/ChunkNorrisApplicationTests.java
@@ -0,0 +1,13 @@
+package github.mralmostcool.chunk_norris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChunkNorrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
